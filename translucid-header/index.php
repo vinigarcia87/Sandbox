a42eb46efe2616679956c43b1922a84db0645618
Vinicius Garcia <vinigarcia87@gmail.com>
2015-04-24 00:52:47 -0300

    - Imagem svg translucida;

---
+++ translucid-header/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Bootstrap 3 Modals</title>
+		
+   		<!--[if lt IE 9]><script src="./assets/vendor/html5shiv/3.7.2/html5shiv.js"></script><![endif]-->
+		<!--[if lt IE 9]><script src="./assets/vendor/respond/1.4.2/respond.min.js"></script><![endif]-->
+   
+        <script src="./assets/vendor/jquery/1.10.2/jquery.min.js"></script>  
+
+        <link rel="stylesheet" href="./assets/vendor/bootstrap/3.3.4/css/bootstrap.min.css"/>
+        <script src="./assets/vendor/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+		<style>
+			body {
+				width:100%;
+				height: 100%;
+				margin: 0 auto;
+				background: #000 url(http://storms.com.br/wp-content/themes/storms2015-1/images/bgFundo.jpg) fixed repeat;
+			}
+			header {
+				height: 50px;
+				width: 100%;
+				
+				background-attachment: fixed;
+				background-color: rgba(255, 255, 255, 0.0);
+				background-image: url("./assets/img/translucid.svg");
+				background-position: center center;
+				background-repeat: repeat;
+				background-size: 0.5em 0.5em;
+			}
+			.bg, .logo {
+				width: 70px;
+				height: 100px;
+				margin: 0 auto;
+				text-align: center;
+			}
+			.logo {
+				background-attachment: fixed;
+				background-color: rgba(255, 255, 255, 0.0);
+				background-image: url("./assets/img/translucid.svg");
+				background-position: center center;
+				background-repeat: repeat;
+				background-size: 0.5em 0.5em;
+			}
+		</style>
+		
+		<script>
+			$(function() {
+				
+			});
+		</script>
+	</head>
+	<body>
+		<header>
+			<div class="bg">
+				<div class="logo">Loucura</div>
+			</div>
+		</header>
+	</body>
+ </html>
